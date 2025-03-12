f12ba7ebe72eda8cf82b02495a62aed6e026255e
Lima <130940527+Gui1herme1ima@users.noreply.github.com>
2025-03-12 07:03:28 -0300

    Atualizar o routes.py correta

--- app/routes.py
+++ app/routes.py
@@ -45,7 +45,7 @@ def start_clicks():
                 url,
                 headers=headers,
                 proxies={"http": proxy, "https": proxy},
-                timeout=time+10,
+                timeout=tempo+10,
                 verify=False
             )
             if response.status_code == 200:
