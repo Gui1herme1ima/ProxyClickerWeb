b69c5d68491f736960b888f8b9d704a8579da3d2
Lima <130940527+Gui1herme1ima@users.noreply.github.com>
2025-03-12 10:32:55 -0300

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -47,7 +47,7 @@ def start_clicks():
                 timeout=60,
                 verify=False
             )
-            if response.status_code == 200:
+            if response.status_code == 200 or response.status_code == 403:
                 cliques_concluidos += 1
                 message = f"Clique concluído com sucesso!"
             else:
