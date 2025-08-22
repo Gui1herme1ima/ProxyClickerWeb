e7420dc0d9b58871b25aab3fa4bc4ac989e93087
Lima <guilimam164@gmail.com>
2025-08-22 09:05:54 -0300

    Update proxies.py

--- proxies.py
+++ proxies.py
@@ -6,42 +6,42 @@ proxies_list = [
     {
         "usuario": "user-sp64heqxiv-sessionduration-1",
         "senha": "Nxa+pD4n2s8tUztUh7",
-        "host": "br.smartproxy.com",
+        "host": "br.decodo.com",
         "porta": 10001,
         "id": "Proxy 1"
     },
     {
         "usuario": "user-sp64heqxiv-sessionduration-1",
         "senha": "Nxa+pD4n2s8tUztUh7",
-        "host": "br.smartproxy.com",
+        "host": "br.decodo.com",
         "porta": 10002,
         "id": "Proxy 2"
     },
     {
         "usuario": "user-sp64heqxiv-sessionduration-1",
         "senha": "Nxa+pD4n2s8tUztUh7",
-        "host": "br.smartproxy.com",
+        "host": "br.decodo.com",
         "porta": 10003,
         "id": "Proxy 3"
     },
     {
         "usuario": "user-sp64heqxiv-sessionduration-1",
         "senha": "Nxa+pD4n2s8tUztUh7",
-        "host": "br.smartproxy.com",
+        "host": "br.decodo.com",
         "porta": 10004,
         "id": "Proxy 4"
     },
     {
         "usuario": "user-sp64heqxiv-sessionduration-1",
         "senha": "Nxa+pD4n2s8tUztUh7",
-        "host": "br.smartproxy.com",
+        "host": "br.decodo.com",
         "porta": 10005,
         "id": "Proxy 5"
     },
     {
         "usuario": "user-sp64heqxiv-sessionduration-1",
         "senha": "Nxa+pD4n2s8tUztUh7",
-        "host": "br.smartproxy.com",
+        "host": "br.decodo.com",
         "porta": 10006,
         "id": "Proxy 6"
     },
@@ -55,301 +55,301 @@ proxies_list = [
     {
         "usuario": "user-sp64heqxiv-sessionduration-1",
         "senha": "Nxa+pD4n2s8tUztUh7",
-        "host": "br.smartproxy.com",
+        "host": "br.decodo.com",
         "porta": 10008,
         "id": "Proxy 8"
     },
     {
         "usuario": "user-sp64heqxiv-sessionduration-1",
         "senha": "Nxa+pD4n2s8tUztUh7",
-        "host": "br.smartproxy.com",
+        "host": "br.decodo.com",
         "porta": 10009,
         "id": "Proxy 9"
     },
     {
         "usuario": "user-sp64heqxiv-sessionduration-1",
         "senha": "Nxa+pD4n2s8tUztUh7",
-        "host": "br.smartproxy.com",
+        "host": "br.decodo.com",
         "porta": 10010,
         "id": "Proxy 10"
     },
     {
         "usuario": "user-sp64heqxiv-sessionduration-1",
         "senha": "Nxa+pD4n2s8tUztUh7",
-        "host": "br.smartproxy.com",
+        "host": "br.decodo.com",
         "porta": 10011,
         "id": "Proxy 11"
     },
     {
         "usuario": "user-sp64heqxiv-sessionduration-1",
         "senha": "Nxa+pD4n2s8tUztUh7",
-        "host": "br.smartproxy.com",
+        "host": "br.decodo.com",
         "porta": 10012,
         "id": "Proxy 12"
     },
     {
         "usuario": "user-sp64heqxiv-sessionduration-1",
         "senha": "Nxa+pD4n2s8tUztUh7",
-        "host": "br.smartproxy.com",
+        "host": "br.decodo.com",
         "porta": 10013,
         "id": "Proxy 13"
     },
     {
         "usuario": "user-sp64heqxiv-sessionduration-1",
         "senha": "Nxa+pD4n2s8tUztUh7",
-        "host": "br.smartproxy.com",
+        "host": "br.decodo.com",
         "porta": 10014,
         "id": "Proxy 14"
     },
     {
         "usuario": "user-sp64heqxiv-sessionduration-1",
         "senha": "Nxa+pD4n2s8tUztUh7",
-        "host": "br.smartproxy.com",
+        "host": "br.decodo.com",
         "porta": 10015,
         "id": "Proxy 15"
     },
     {
         "usuario": "user-sp64heqxiv-sessionduration-1",
         "senha": "Nxa+pD4n2s8tUztUh7",
-        "host": "br.smartproxy.com",
+        "host": "br.decodo.com",
         "porta": 10016,
         "id": "Proxy 16"
     },
     {
         "usuario": "user-sp64heqxiv-sessionduration-1",
         "senha": "Nxa+pD4n2s8tUztUh7",
-        "host": "br.smartproxy.com",
+        "host": "br.decodo.com",
         "porta": 10017,
         "id": "Proxy 17"
     },
     {
         "usuario": "user-sp64heqxiv-sessionduration-1",
         "senha": "Nxa+pD4n2s8tUztUh7",
-        "host": "br.smartproxy.com",
+        "host": "br.decodo.com",
         "porta": 10018,
         "id": "Proxy 18"
     },
     {
         "usuario": "user-sp64heqxiv-sessionduration-1",
         "senha": "Nxa+pD4n2s8tUztUh7",
-        "host": "br.smartproxy.com",
+        "host": "br.decodo.com",
         "porta": 10019,
         "id": "Proxy 19"
     },
     {
         "usuario": "user-sp64heqxiv-sessionduration-1",
         "senha": "Nxa+pD4n2s8tUztUh7",
-        "host": "br.smartproxy.com",
+        "host": "br.decodo.com",
         "porta": 10020,
         "id": "Proxy 20"
     },
     {
         "usuario": "user-sp64heqxiv-sessionduration-1",
         "senha": "Nxa+pD4n2s8tUztUh7",
-        "host": "br.smartproxy.com",
+        "host": "br.decodo.com",
         "porta": 10021,
         "id": "Proxy 21"
     },
     {
         "usuario": "user-sp64heqxiv-sessionduration-1",
         "senha": "Nxa+pD4n2s8tUztUh7",
-        "host": "br.smartproxy.com",
+        "host": "br.decodo.com",
         "porta": 10022,
         "id": "Proxy 22"
     },
     {
         "usuario": "user-sp64heqxiv-sessionduration-1",
         "senha": "Nxa+pD4n2s8tUztUh7",
-        "host": "br.smartproxy.com",
+        "host": "br.decodo.com",
         "porta": 10023,
         "id": "Proxy 23"
     },
     {
         "usuario": "user-sp64heqxiv-sessionduration-1",
         "senha": "Nxa+pD4n2s8tUztUh7",
-        "host": "br.smartproxy.com",
+        "host": "br.decodo.com",
         "porta": 10024,
         "id": "Proxy 24"
     },
     {
         "usuario": "user-sp64heqxiv-sessionduration-1",
         "senha": "Nxa+pD4n2s8tUztUh7",
-        "host": "br.smartproxy.com",
+        "host": "br.decodo.com",
         "porta": 10025,
         "id": "Proxy 25"
     },
     {
         "usuario": "user-sp64heqxiv-sessionduration-1",
         "senha": "Nxa+pD4n2s8tUztUh7",
-        "host": "br.smartproxy.com",
+        "host": "br.decodo.com",
         "porta": 10026,
         "id": "Proxy 26"
     },
     {
         "usuario": "user-sp64heqxiv-sessionduration-1",
         "senha": "Nxa+pD4n2s8tUztUh7",
-        "host": "br.smartproxy.com",
+        "host": "br.decodo.com",
         "porta": 10027,
         "id": "Proxy 27"
     },
     {
         "usuario": "user-sp64heqxiv-sessionduration-1",
         "senha": "Nxa+pD4n2s8tUztUh7",
-        "host": "br.smartproxy.com",
+        "host": "br.decodo.com",
         "porta": 10028,
         "id": "Proxy 28"
     },
     {
         "usuario": "user-sp64heqxiv-sessionduration-1",
         "senha": "Nxa+pD4n2s8tUztUh7",
-        "host": "br.smartproxy.com",
+        "host": "br.decodo.com",
         "porta": 10029,
         "id": "Proxy 29"
     },
     {
         "usuario": "user-sp64heqxiv-sessionduration-1",
         "senha": "Nxa+pD4n2s8tUztUh7",
-        "host": "br.smartproxy.com",
+        "host": "br.decodo.com",
         "porta": 10030,
         "id": "Proxy 30"
     },
     {
         "usuario": "user-sp64heqxiv-sessionduration-1",
         "senha": "Nxa+pD4n2s8tUztUh7",
-        "host": "br.smartproxy.com",
+        "host": "br.decodo.com",
         "porta": 10031,
         "id": "Proxy 31"
     },
     {
         "usuario": "user-sp64heqxiv-sessionduration-1",
         "senha": "Nxa+pD4n2s8tUztUh7",
-        "host": "br.smartproxy.com",
+        "host": "br.decodo.com",
         "porta": 10032,
         "id": "Proxy 32"
     },
     {
         "usuario": "user-sp64heqxiv-sessionduration-1",
         "senha": "Nxa+pD4n2s8tUztUh7",
-        "host": "br.smartproxy.com",
+        "host": "br.decodo.com",
         "porta": 10033,
         "id": "Proxy 33"
     },
     {
         "usuario": "user-sp64heqxiv-sessionduration-1",
         "senha": "Nxa+pD4n2s8tUztUh7",
-        "host": "br.smartproxy.com",
+        "host": "br.decodo.com",
         "porta": 10034,
         "id": "Proxy 34"
     },
     {
         "usuario": "user-sp64heqxiv-sessionduration-1",
         "senha": "Nxa+pD4n2s8tUztUh7",
-        "host": "br.smartproxy.com",
+        "host": "br.decodo.com",
         "porta": 10035,
         "id": "Proxy 35"
     },
     {
         "usuario": "user-sp64heqxiv-sessionduration-1",
         "senha": "Nxa+pD4n2s8tUztUh7",
-        "host": "br.smartproxy.com",
+        "host": "br.decodo.com",
         "porta": 10036,
         "id": "Proxy 36"
     },
     {
         "usuario": "user-sp64heqxiv-sessionduration-1",
         "senha": "Nxa+pD4n2s8tUztUh7",
-        "host": "br.smartproxy.com",
+        "host": "br.decodo.com",
         "porta": 10037,
         "id": "Proxy 37"
     },
     {
         "usuario": "user-sp64heqxiv-sessionduration-1",
         "senha": "Nxa+pD4n2s8tUztUh7",
-        "host": "br.smartproxy.com",
+        "host": "br.decodo.com",
         "porta": 10038,
         "id": "Proxy 38"
     },
     {
         "usuario": "user-sp64heqxiv-sessionduration-1",
         "senha": "Nxa+pD4n2s8tUztUh7",
-        "host": "br.smartproxy.com",
+        "host": "br.decodo.com",
         "porta": 10039,
         "id": "Proxy 39"
     },
     {
         "usuario": "user-sp64heqxiv-sessionduration-1",
         "senha": "Nxa+pD4n2s8tUztUh7",
-        "host": "br.smartproxy.com",
+        "host": "br.decodo.com",
         "porta": 10040,
         "id": "Proxy 40"
     },
     {
         "usuario": "user-sp64heqxiv-sessionduration-1",
         "senha": "Nxa+pD4n2s8tUztUh7",
-        "host": "br.smartproxy.com",
+        "host": "br.decodo.com",
         "porta": 10041,
         "id": "Proxy 41"
     },
     {
         "usuario": "user-sp64heqxiv-sessionduration-1",
         "senha": "Nxa+pD4n2s8tUztUh7",
-        "host": "br.smartproxy.com",
+        "host": "br.decodo.com",
         "porta": 10042,
         "id": "Proxy 42"
     },
     {
         "usuario": "user-sp64heqxiv-sessionduration-1",
         "senha": "Nxa+pD4n2s8tUztUh7",
-        "host": "br.smartproxy.com",
+        "host": "br.decodo.com",
         "porta": 10043,
         "id": "Proxy 43"
     },
     {
         "usuario": "user-sp64heqxiv-sessionduration-1",
         "senha": "Nxa+pD4n2s8tUztUh7",
-        "host": "br.smartproxy.com",
+        "host": "br.decodo.com",
         "porta": 10044,
         "id": "Proxy 44"
     },
     {
         "usuario": "user-sp64heqxiv-sessionduration-1",
         "senha": "Nxa+pD4n2s8tUztUh7",
-        "host": "br.smartproxy.com",
+        "host": "br.decodo.com",
         "porta": 10045,
         "id": "Proxy 45"
     },
     {
         "usuario": "user-sp64heqxiv-sessionduration-1",
         "senha": "Nxa+pD4n2s8tUztUh7",
-        "host": "br.smartproxy.com",
+        "host": "br.decodo.com",
         "porta": 10046,
         "id": "Proxy 46"
     },
     {
         "usuario": "user-sp64heqxiv-sessionduration-1",
         "senha": "Nxa+pD4n2s8tUztUh7",
-        "host": "br.smartproxy.com",
+        "host": "br.decodo.com",
         "porta": 10047,
         "id": "Proxy 47"
     },
     {
         "usuario": "user-sp64heqxiv-sessionduration-1",
         "senha": "Nxa+pD4n2s8tUztUh7",
-        "host": "br.smartproxy.com",
+        "host": "br.decodo.com",
         "porta": 10048,
         "id": "Proxy 48"
     },
     {
         "usuario": "user-sp64heqxiv-sessionduration-1",
         "senha": "Nxa+pD4n2s8tUztUh7",
-        "host": "br.smartproxy.com",
+        "host": "br.decodo.com",
         "porta": 10049,
         "id": "Proxy 49"
     },
     {
         "usuario": "user-sp64heqxiv-sessionduration-1",
         "senha": "Nxa+pD4n2s8tUztUh7",
-        "host": "br.smartproxy.com",
+        "host": "br.decodo.com",
         "porta": 10050,
         "id": "Proxy 50"
     },
